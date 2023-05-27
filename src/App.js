4335c99509857e1711b0df2b32fa46320a6776b8
amantai <949898ai@gmail.com>
2023-05-27 14:10:59 +0600

    done hw4

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class NameForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      phone: "",
+      email: ""
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    alert(`Name: ${this.state.name}\nPhone: ${this.state.phone}\nEmail: ${this.state.email}`);
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input type="text" name="name" value={this.state.name} onChange={this.handleChange} />
+        </label>
+        <label>
+          Phone:
+          <input type="number" name="phone" value={this.state.phone} onChange={this.handleChange} />
+        </label>
+        <label>
+          Email:
+          <input type="email" name="email" value={this.state.email} onChange={this.handleChange} />
+        </label>
+        <input className="submit" type="submit" value="Send" />
+      </form>
+    );
+  }
 }
 
-export default App;
+export default NameForm;
